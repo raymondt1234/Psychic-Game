6330d9ba08c29f27e7f7c5eaf87b86cc7a20aa0a
raymondt1234 <raymondt1234@gmail.com>
2019-08-26 15:12:50 -0700

    Working on game loop...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,35 @@
+
 function randomLetter() {
 
     return String.fromCharCode(97 + Math.floor(Math.random() * 26));
 
 }
+
+let playerWins = false;
 let wins = 0;
 let losses = 0;
 let guesses = 9;
-While (guesses > 0) {
+
+While(guesses > 0 || playerWins) {
+
     let guessedLetter = [];
-    document.onkeyup = 
+    let computerLetter = randomLetter();
+
+    document.onkeyup = function (event) {
+        let keyPressed = event.key;
+        guessedLetter.push(keyPressed);
+        for (let i = 0; i < guessedLetter.length; i++) {
+            document.write(guessedLetter);
+            guesses--;
+            if (keyPressed === computerLetter) {
+                wins++;
+                playerWins = true;
+            } else {
+                losses++;
+            }
+            document.write("Wins: " + wins);
+            document.write("Losses: " + losses);
+            document.write("Guesses Left: " + guesses);
+        }
+    }
 }
