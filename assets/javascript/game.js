f49f3725febebb3ca5cd7b7849601f2e192c19c1
raymondt1234 <raymondt1234@gmail.com>
2019-08-27 11:45:45 -0700

    Changed the if statements to fix logic error

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,38 +4,53 @@ function randomLetter() {
     return String.fromCharCode(97 + Math.floor(Math.random() * 26));
 
 }
-function printScoreboard(){
-    document.write("<h1>The Psychic Game</h1>");
-    document.write("<h2>Guess what letter I'm thinking of</h2>");
-    document.write("<h2>Wins: " + wins + "</h2>");
-    document.write("<h2>Losses: " + losses + "</h2>");
-    document.write("<h2>Guesses: " + guesses + "</h2>");
+function printScoreboard() {
+
+    console.log("The Psychic Game");
+    console.log("Guess what letter I'm thinking of");
+    console.log("Wins: " + wins);
+    console.log("Losses: " + losses);
+    console.log("Guesses: " + guesses);
 }
 
 let playerWins = false;
 let wins = 0;
 let losses = 0;
 let guesses = 9;
-let computerLetter = "z" // randomLetter();
+let computerLetter =  randomLetter();
+console.log("Computer Letter: " + computerLetter);
 
 printScoreboard();
 
 document.onkeyup = function (event) {
-    let keyPressed = event.key;
-    console.log("Key " + keyPressed);
-    
-    printScoreboard();
-
-    if (keyPressed === computerLetter) {
-        wins++;
-        guesses = 9;
-        console.log("player wins"); //
-    } else if (keyPressed !== computerLetter) {
-        losses++;
-        guesses--;
-        console.log("computer wins"); //
+    var keyPressed = event.key;
+    console.log("Key pressed: " + keyPressed);
+    if (guesses > 0) {
+        if (keyPressed === computerLetter) {
+            wins++;
+            guesses = 9;
+            computerLetter =  randomLetter();
+            console.log("Computer Letter: " + computerLetter);
+        } else if (keyPressed !== computerLetter) {
+            guesses--;
+            if (guesses === 0) {
+                guesses = 9;
+                losses++;
+                computerLetter =  randomLetter();
+                console.log("Computer Letter: " + computerLetter);
+            }
+        }
     } else {
-        guesses--;
+        losses++;
+        guesses = 9;
+        computerLetter =  randomLetter();
+        console.log("Computer Letter: " + computerLetter);
     }
+    printScoreboard();
 }
 
+// document.write("<h1>The Psychic Game</h1>");
+    // document.write("<h2>Guess what letter I'm thinking of</h2>");
+    // document.write("<h2>Wins: " + wins + "</h2>");
+    // document.write("<h2>Losses: " + losses + "</h2>");
+    // document.write("<h2>Guesses: " + guesses + "</h2>");
