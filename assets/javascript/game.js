4bc43eea3c1f9a0f06ca8047e59200023a7cbad9
raymondt1234 <raymondt1234@gmail.com>
2019-08-26 18:02:31 -0700

    Code rewritten

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,32 +4,38 @@ function randomLetter() {
     return String.fromCharCode(97 + Math.floor(Math.random() * 26));
 
 }
+function printScoreboard(){
+    document.write("<h1>The Psychic Game</h1>");
+    document.write("<h2>Guess what letter I'm thinking of</h2>");
+    document.write("<h2>Wins: " + wins + "</h2>");
+    document.write("<h2>Losses: " + losses + "</h2>");
+    document.write("<h2>Guesses: " + guesses + "</h2>");
+}
 
 let playerWins = false;
 let wins = 0;
 let losses = 0;
 let guesses = 9;
+let computerLetter = "z" // randomLetter();
 
-While(guesses > 0 || playerWins) {
+printScoreboard();
 
-    let guessedLetter = [];
-    let computerLetter = randomLetter();
+document.onkeyup = function (event) {
+    let keyPressed = event.key;
+    console.log("Key " + keyPressed);
+    
+    printScoreboard();
 
-    document.onkeyup = function (event) {
-        let keyPressed = event.key;
-        guessedLetter.push(keyPressed);
-        for (let i = 0; i < guessedLetter.length; i++) {
-            document.write(guessedLetter);
-            guesses--;
-            if (keyPressed === computerLetter) {
-                wins++;
-                playerWins = true;
-            } else {
-                losses++;
-            }
-            document.write("Wins: " + wins);
-            document.write("Losses: " + losses);
-            document.write("Guesses Left: " + guesses);
-        }
+    if (keyPressed === computerLetter) {
+        wins++;
+        guesses = 9;
+        console.log("player wins"); //
+    } else if (keyPressed !== computerLetter) {
+        losses++;
+        guesses--;
+        console.log("computer wins"); //
+    } else {
+        guesses--;
     }
 }
+
