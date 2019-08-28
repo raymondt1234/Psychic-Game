f5afa2801186aea92865ae58c5a9deac83d84f52
raymondt1234 <raymondt1234@gmail.com>
2019-08-27 18:48:48 -0700

    added missing code to track letters chosen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,7 @@ function printScoreboard() {
     document.getElementById("wins").textContent = wins;
     document.getElementById("losses").textContent = losses;
     document.getElementById("guesses").textContent = guesses;
+    document.getElementById("letters").textContent = letters;
     console.log("Computer Letter: " + computerLetter);
 }
 
@@ -15,26 +16,28 @@ let playerWins = false;
 let wins = 0;
 let losses = 0;
 let guesses = 9;
-let computerLetter =  randomLetter();
-
+let letters = [];
+let computerLetter = randomLetter();
 
 
 printScoreboard();
 
 document.onkeyup = function (event) {
     var keyPressed = event.key;
-    console.log("Key pressed: " + keyPressed);
+    letters.push(keyPressed);
 
     if (guesses > 0) {
+        
         if (keyPressed === computerLetter) {
             wins++;
             guesses = 9;
             computerLetter = randomLetter();
         } else if (keyPressed !== computerLetter) {
             guesses--;
+            
             if (guesses === 0) {
-                guesses = 9;
                 losses++;
+                guesses = 9;
                 computerLetter = randomLetter();
             }
         }
@@ -43,6 +46,6 @@ document.onkeyup = function (event) {
         guesses = 9;
         computerLetter = randomLetter();
     }
-    
+
     printScoreboard();
 }
