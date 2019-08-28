f37b371a08e36520c2044a170127d07bc820ab44
raymondt1234 <raymondt1234@gmail.com>
2019-08-27 18:15:20 -0700

    Fixed bugs? and made html score update through JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,12 +5,10 @@ function randomLetter() {
 
 }
 function printScoreboard() {
-
-    console.log("The Psychic Game");
-    console.log("Guess what letter I'm thinking of");
-    console.log("Wins: " + wins);
-    console.log("Losses: " + losses);
-    console.log("Guesses: " + guesses);
+    document.getElementById("wins").textContent = wins;
+    document.getElementById("losses").textContent = losses;
+    document.getElementById("guesses").textContent = guesses;
+    console.log("Computer Letter: " + computerLetter);
 }
 
 let playerWins = false;
@@ -18,39 +16,33 @@ let wins = 0;
 let losses = 0;
 let guesses = 9;
 let computerLetter =  randomLetter();
-console.log("Computer Letter: " + computerLetter);
+
+
 
 printScoreboard();
 
 document.onkeyup = function (event) {
     var keyPressed = event.key;
     console.log("Key pressed: " + keyPressed);
+
     if (guesses > 0) {
         if (keyPressed === computerLetter) {
             wins++;
             guesses = 9;
-            computerLetter =  randomLetter();
-            console.log("Computer Letter: " + computerLetter);
+            computerLetter = randomLetter();
         } else if (keyPressed !== computerLetter) {
             guesses--;
             if (guesses === 0) {
                 guesses = 9;
                 losses++;
-                computerLetter =  randomLetter();
-                console.log("Computer Letter: " + computerLetter);
+                computerLetter = randomLetter();
             }
         }
     } else {
         losses++;
         guesses = 9;
-        computerLetter =  randomLetter();
-        console.log("Computer Letter: " + computerLetter);
+        computerLetter = randomLetter();
     }
+    
     printScoreboard();
 }
-
-// document.write("<h1>The Psychic Game</h1>");
-    // document.write("<h2>Guess what letter I'm thinking of</h2>");
-    // document.write("<h2>Wins: " + wins + "</h2>");
-    // document.write("<h2>Losses: " + losses + "</h2>");
-    // document.write("<h2>Guesses: " + guesses + "</h2>");
